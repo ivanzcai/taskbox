3a684ee9a90555d286605d1568ad8bb9fedbfe68
ivan_z_cai <ivan.c@geoop.com>
2021-02-15 14:58:17 +1100

    some changes

---
+++ src/components/Task.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+export default function Task({ task: { id, title, state}, onArchiveTask, onPinTask })  {
+    return (
+      <div className="list-item">
+        <input type="text" value={title} readOnly={true} />
+      </div>
+    );
+  }
